d15511843cb4c0021eb11923e15c03546c486566
KamilaStuglik <77364670+KamilaStuglik@users.noreply.github.com>
2022-01-30 00:14:32 +0100

    displayTemperature function added

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ function displayTemperature(response){
 }
 
 let apiKey = "c064a1d958e6c2a52bab92fa47962081";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=c064a1d958e6c2a52bab92fa47962081`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&units=metric&appid=${apiKey}`;
 
 
-axios.get(`${apiUrl}&appid=${apiKey}`).then(displayTemperature);
+axios.get(apiUrl).then(displayTemperature);

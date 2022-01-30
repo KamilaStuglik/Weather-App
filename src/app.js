2699ff76efc4055ceff4c397e85ac7d7fe1fc399
KamilaStuglik <77364670+KamilaStuglik@users.noreply.github.com>
2022-01-30 11:55:00 +0100

    Function formatDate added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let day = date.getDay();
+    return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response){
     console.log(response.data.wind);
     let temperatureElement = document.querySelector("#temp");
@@ -5,12 +13,14 @@ function displayTemperature(response){
     let descriptionElement = document.querySelector("#weather-description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
         
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "c064a1d958e6c2a52bab92fa47962081";

1836699ed4a069e844ec00c0806f4379185d5d9d
KamilaStuglik <77364670+KamilaStuglik@users.noreply.github.com>
2022-01-30 10:40:48 +0100

    Added elements to displayTemperature function

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 function displayTemperature(response){
-    console.log(response.data.main.temp);
+    console.log(response.data.wind);
+    let temperatureElement = document.querySelector("#temp");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#weather-description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+        
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "c064a1d958e6c2a52bab92fa47962081";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&units=metric&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Havana&units=metric&appid=${apiKey}`;
 
 
 axios.get(apiUrl).then(displayTemperature);
